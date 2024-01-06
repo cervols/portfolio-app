b61551e636f3cd8435ebf681cace5198627cf741
cervols <paprikamail@gmail.com>
2024-01-06 18:19:19 +0100

    Refactor navigation & implement active page status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,9 +19,48 @@ module ApplicationHelper
     TzViewTool::Renderer.copyright("Tania Zavediuk", "All rights reserved")
   end
 
+  def nav_generator(tag_type, style)
+    nav_links = ""
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active?(item[:url])}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+      {
+        url: about_path,
+        title: "About Me"
+      },
+      {
+        url: contact_path,
+        title: "Contact"
+      },
+      {
+        url: blogs_path,
+        title: "Blog"
+      },
+      {
+        url: portfolios_path,
+        title: "Portfolio"
+      },
+    ]
+  end
+
   private
 
     def greeting
       "Thanks for visiting me from #{session[:source]}!"
+    end
+
+    def active?(path)
+      "active" if current_page?(path)
     end
 end
